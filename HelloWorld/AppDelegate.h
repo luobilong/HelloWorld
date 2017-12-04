19451a8fc7d437fff4a9677890379d7b3a5efee3
ljg <ljg@ljgdeMacBook-Pro.local>
2017-12-05 00:33:32 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by ljg on 2017/12/5.
+//  Copyright © 2017年 ljg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
